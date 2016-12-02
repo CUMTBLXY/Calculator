29faf3c8887c10e474ebdb8033b751589cb89c3d
unknown <350535629@qq.com>
2016-12-02 13:17:03 +0800

    The function of clear scream,but have bug.

--- src/js/test21/caculator.js
+++ src/js/test21/caculator.js
@@ -14,36 +14,43 @@ document.addEventListener("DOMContentLoaded",function(){
 
     var result = 0;
 
-    for (var i=0;i<number.length;i++) {
+    for (var i=0;i<number.length;i++) {//数字键
         number[i].addEventListener("click", function () {
 
                 var display = document.getElementById("result");
                 var addnum = this.innerHTML;
-                display.value = display.value+addnum;
+                yunsuan(addnum);
+
         }, false);
     }
 
-    for (var i=0;i<operators.length;i++) {
+    for (var i=0;i<operators.length;i++) {//运算符
 
         operators[i].addEventListener("click", function () {
 
             var display = document.getElementById("result");
             var addnum = this.innerHTML;
-            display.value = addnum;
+            yunsuan(addnum);
+
 
         }, false);
     }
 
-    function yunsuan(input){
+    function yunsuan(input){//输入时对显示屏的清空
         var compnumber = new RegExp([0-9]);
-        var compsigh = new RegExp([+-×÷])
-        if (compnumber.test(input))
-            var display =document.getElementById("result");
-            if (compsigh.test(display)){
-                display.value = now;
-        }
-            else{
+        var compsigh = new RegExp[+-×÷];
+        var display = document.getElementById("result");
+        if (compnumber.test(input)) {//如果输入的是数字的话，先判断前一个是否是数字，还是运算符号
+            if (compsigh.test(display)) {//是运算符号，就清空显示屏
+                display.value = input;
 
+            }
+            else {//是数字就继续累加
+                display.value = display.value+input;
+            }
+        }
+        if (compsigh.test(input)){//如果输入的是运算符就清空
+            display.value = input;
         }
 
     }
